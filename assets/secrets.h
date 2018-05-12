7a45801bade3fe9b9c0cbc1a1aea5657fbc7ef82
Jose Polanco <polancoguramy@gmail.com>
2018-05-12 20:48:36 -0300

    archivos con actualizaciones generadas por el software

--- assets/secrets.h
+++ assets/secrets.h
@@ -3,10 +3,10 @@
 
 #define SSID "EngieFactory"
 #define PASSWORD "3ng135855"
-#define AP_SSID "CLIMO-2003"
+#define AP_SSID "CLIMO-2000"
 #define AP_PASS "3ng135855"
-#define KEY "tEOVPo6w8mIobqvk"
-#define MAC "qCuQ9qb"
-#define CLIMOID "2003"
+#define KEY "Dr6bDJEIS6TaEaI2"
+#define MAC "f2V4l63"
+#define CLIMOID "2000"
 
 #endif
--- assets/spiffs_image.sh
+++ assets/spiffs_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 VESAT_mkspiffs_ESP32/mkspiffs -c Climo-ESPWeb -d 5 -b 4096 -p 256 -s 0x10000 spiffs.img
-python ~/esp/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0  --baud 115200 write_flash -u --flash_mode dio --flash_freq 40m --flash_size 1MB 0xE0000 spiffs.img
+python ~/esp/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB10  --baud 115200 write_flash -u --flash_mode dio --flash_freq 40m --flash_size 1MB 0xE0000 spiffs.img

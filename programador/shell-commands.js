c01af80529d2c2633dae6530cd35fd09ed5ee1ba
Jose Polanco <polancoguramy@gmail.com>
2018-05-14 22:19:11 -0400

    Aplicacion con implementacion de comandos para flash y config

--- programador/shell-commands.js
+++ programador/shell-commands.js
@@ -1,53 +1,49 @@
-const { exec } = require('child_process');
+const { exec } = require('child_process')
 
 document.getElementById('flashing').addEventListener('click', function () {
 
-    alert("Esto inicia el flasheo");
-    exec('export PATH="$PATH:$HOME/esp/xtensa-esp32-elf/bin"', (error, stdout, stderr) => {
-        if (error) { 
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-    exec('make /home/urayi/Documents/climo/CLIMO_Pagina_Configuracion/', (error, stdout, stderr) => {
+    alert("Esto inicia el flasheo")
+    document.getElementById('flashing').disabled = true
+    document.getElementById('config').disabled = true
+    console.log('Flashing...')
+    exec('../../flasher', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
+            document.getElementById('flashing').disabled = false
+            document.getElementById('config').disabled = false
             return;
         }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-    exec('make /home/urayi/Documents/climo/CLIMO_Pagina_Configuracion/','flash', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+        if (stdout && !(stderr == '')) {
+            alert("Dispositivo configurado")
+        } else {
+            alert("Es posible que ocurriera un error:" + stderr)
         }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
+        document.getElementById('flashing').disabled = false
+        document.getElementById('config').disabled = false
+    })
+
 }, false);
 
 document.getElementById('config').addEventListener('click', function () {
 
     alert("Esto configura y carga la pagina web");
-    /* exec('cd /home/urayi/Documents/climo/CLIMO_Pagina_Configuracion/VESAT_mkspiffs_ESP32/ make dist', (error, stdout, stderr) => {
+    document.getElementById('flashing').disabled = true
+    document.getElementById('config').disabled = true
+    console.log('Flashing...')
+    exec('../../config', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
+            document.getElementById('flashing').disabled = false
+            document.getElementById('config').disabled = false
             return;
         }
         console.log(`stdout: ${stdout}`);
+        alert("Dispositivo configurado");
         console.log(`stderr: ${stderr}`);
-    }); */
+        document.getElementById('flashing').disabled = false
+        document.getElementById('config').disabled = false
+    })
 
-    exec('/home/urayi/Documents/climo/CLIMO_Pagina_Configuracion/spiffs_image', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-
-}, false);
+}, false)
